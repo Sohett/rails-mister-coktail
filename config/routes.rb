c9f49193bcf21eff9a5448bba8d887825c191873
Thomas Sohet <sohet.Thomas@gmail.com>
2017-11-02 16:06:40 +0100

    controller & views added for the cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:create]
+  end
+  delete 'doses/:id', to: 'doses#destroy', as: "dose"
+
+  root 'cocktails#index'
 end
