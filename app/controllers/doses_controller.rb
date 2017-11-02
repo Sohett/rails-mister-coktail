9dd9c20dc7583a28ee58f8c1634055e8719bfb81
Thomas Sohet <sohet.Thomas@gmail.com>
2017-11-02 23:34:41 +0100

    prepare pushing for heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,18 +4,15 @@ class DosesController < ApplicationController
   def create
     dose = Dose.new(dose_params)
     dose.cocktail = @cocktail
-    if dose.save
-      redirect_to cocktail_path(@cocktail)
-    else
-      render :new
-    end
+    dose.save
+    redirect_to cocktail_path(@cocktail)
   end
 
   def destroy
     dose = Dose.find(params.require(:id))
-    @cocktail = dose.cocktail
+    cocktail = dose.cocktail
     dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(cocktail)
   end
 
   private
